131e8db746ccb5613cc9a971704655b8014319ad
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-04-20 13:45:18 +0200

    Update test suite
    Update test suite to run its own instance of ganache and deploy
    contracts from sub-directory truffle.
    
    VTX ?

--- test/test.js
+++ test/test.js
@@ -1,5 +1,35 @@
 const expect = require('chai').expect;
+const Ganache = require("ganache-core");
+const { exec } = require('child_process');
+let Server;
 
 describe('VortΞx', function() {
+
+    describe("Init", function() {
+
+        it("Starts an instance of ganache-core", (done) => {
+            Server = Ganache.server();
+            Server.listen(8545, (err) => {
+                done(err);
+            });
+        });
+
+        it("Migrates contracts on temporary ganache session", (done) => {
+            exec("cd test/truffle && ../../node_modules/.bin/truffle migrate --reset && cd ../..", (err, stdout, stderr) => {
+                console.log(stdout);
+                console.error(stderr);
+                done(err);
+            });
+        }).timeout(100000);
+
+    });
+
+    describe("Cleaning ...", () => {
+
+        it("Stops the previously started server", () => {
+            Server.close();
+        });
+
+    })
 
 });
