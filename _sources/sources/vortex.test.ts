7a7e8eb4d9fc0176e83ecd1574d97bb356204e33
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-04-23 15:10:51 +0200

    Update Jest test suite
    VTX #11

--- _sources/sources/vortex.test.ts
+++ _sources/sources/vortex.test.ts
@@ -1,8 +1,11 @@
+declare var describe: any;
 declare var test: any;
 declare var expect: any;
 
 import {Vortex} from "./vortex";
 
-test('Construction', () => {
-    expect((new Vortex()).test).toBe("test");
+describe("Vortex", () => {
+    test('Construction', () => {
+        expect((new Vortex()).test).toBe("test");
+    });
 });
