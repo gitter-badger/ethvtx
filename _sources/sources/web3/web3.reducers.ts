3cb406fb32e64294a70491d919c4aa6173310732
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-04-23 14:12:26 +0200

    Add Web3 actions, reducers and sagas
    VTX #11

---
+++ _sources/sources/web3/web3.reducers.ts
@@ -0,0 +1,40 @@
+import {Reducer} from "redux";
+import {Web3State} from "../stateInterface";
+import {Web3Actions, Web3LoadedAction, Web3LoadErrorAction, Web3NetworkErrorAction} from "./web3.actions";
+
+export const web3 : Reducer<Web3State, Web3Actions> = (state: Web3State = {status: 'LOADING'}, action: Web3Actions): Web3State => {
+
+    switch (action.type) {
+
+        case 'LOAD_WEB3':
+            return ({
+                status: 'LOADING'
+            });
+
+        case 'LOADED_WEB3':
+            return ({
+                ...state,
+                status: 'LOADED',
+                network_id: (<Web3LoadedAction>action).networkId,
+                _: (<Web3LoadedAction>action)._
+            });
+
+        case 'LOAD_ERROR_WEB3':
+            return ({
+                ...state,
+                status: 'LOAD_ERROR',
+                error: (<Web3LoadErrorAction>action).error,
+            });
+
+        case 'NETWORK_ERROR_WEB3':
+            return ({
+                ...state,
+                status: 'NETWORK_ERROR',
+                network_id: (<Web3NetworkErrorAction>action).networkId
+            });
+
+        default:
+            return state;
+
+    }
+};
