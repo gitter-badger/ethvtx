03b97deead09b6a4e74c1175ff5e5fc356f83896
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-04-30 18:02:26 +0200

    Remove utils and add selector from reselect
    VTX #24

---
+++ _sources/sources/feed/feed.selectors.ts
@@ -0,0 +1,31 @@
+import {createSelector} from "reselect";
+import {FeedState, State} from "../stateInterface";
+
+const getFeed = (state: State): FeedState[] => state.feed;
+
+export enum FeedType {
+    Transactions = 1,
+    Contracts = 2,
+    Errors = 4,
+}
+
+const FeedTypeLinks: any = {
+    NEW_TRANSACTION: 1,
+    NEW_CONTRACT: 2,
+    NEW_ERROR: 4
+};
+
+export const FeedFilterTransactions = createSelector(getFeed, (feed: FeedState[]): FeedState[] => {
+    return feed.filter((elem: FeedState): boolean => !!((FeedTypeLinks[elem.action] ? FeedTypeLinks[elem.action] : -1) & FeedType.Transactions))
+});
+
+export const FeedFilterContracts = createSelector(getFeed, (feed: FeedState[]): FeedState[] => {
+    return feed.filter((elem: FeedState): boolean => !!((FeedTypeLinks[elem.action] ? FeedTypeLinks[elem.action] : -1) & FeedType.Contracts))
+});
+
+export const FeedFilterErrors = createSelector(getFeed, (feed: FeedState[]): FeedState[] => {
+    return feed.filter((elem: FeedState): boolean => !!((FeedTypeLinks[elem.action] ? FeedTypeLinks[elem.action] : -1) & FeedType.Errors))
+});
+
+
+
