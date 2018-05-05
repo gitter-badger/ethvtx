b5c99b70388e4c604fd4435747291df956052ddf
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-05 15:56:35 +0200

    Hotfix: Explicit exports
    VTX

--- _sources/index.ts
+++ _sources/index.ts
@@ -1,9 +1,86 @@
-export * from './sources/vortex';
-export * from './sources/stateInterface';
-export * from './sources/dummyReducer';
-export * from './sources/web3/web3.actions';
-export * from './sources/tx/tx.actions';
-export * from './sources/contracts/contracts.actions';
-export * from './sources/feed/feed.actions';
-export * from './sources/feed/feed.selectors';
-export * from './sources/contracts/VortexContract';
+export {Vortex} from './sources/vortex';
+export {
+    Web3LoadedState,
+    Web3State,
+    Web3LoadErrorState,
+    Web3LoadingState,
+    Web3NetworkErrorState,
+    FeedState,
+    FeedNewErrorState,
+    FeedNewErrorErrorState,
+    FeedNewContractState,
+    FeedNewTransactionState,
+    TransactionArgumentState,
+    TransactionErrorState,
+    RawTransactionArgumentState,
+    TransactionConfirmedState,
+    TransactionReceiptState,
+    TransactionBroadcastedState,
+    ContractStoreState,
+    ContractAddressesState,
+    TransactionState,
+    ContractInstanceState,
+    State,
+    TransactionStoreState
+} from './sources/stateInterface';
+export {dummyReducer} from './sources/dummyReducer';
+export {
+    Web3LoadedAction,
+    Web3Actions,
+    Web3Load,
+    Web3LoadAction,
+    Web3Loaded,
+    Web3LoadError,
+    Web3LoadErrorAction,
+    Web3NetworkError,
+    Web3NetworkErrorAction
+} from './sources/web3/web3.actions';
+export {
+    TxBroadcastedAction,
+    TxErrorAction,
+    TxConfirmedAction,
+    TxReceiptAction,
+    TxActions,
+    TxBroadcasted,
+    TxConfirmed,
+    TxError,
+    TxReceipt,
+    TxSend,
+    TxSendAction,
+    TxSendRaw,
+    TxSendRawAction
+} from './sources/tx/tx.actions';
+export {
+    ContractLoadAction,
+    ContractVarForceRefreshAction,
+    ContractVarErrorReceivedAction,
+    ContractVarReceivedAction,
+    ContractErrorAction,
+    ContractLoadedAction,
+    ContractLoadingAction,
+    ContractActions,
+    ContractCall,
+    ContractCallAction,
+    ContractError,
+    ContractLoad,
+    ContractLoaded,
+    ContractLoading,
+    ContractSend,
+    ContractSendAction,
+    ContractVarErrorReceived,
+    ContractVarForceRefresh,
+    ContractVarReceived
+} from './sources/contracts/contracts.actions';
+export {
+    FeedNewErrorAction,
+    FeedNewTransaction,
+    FeedNewContract,
+    FeedNewContractAction,
+    FeedNewTransactionAction,
+    FeedActions,
+    FeedNewError
+} from './sources/feed/feed.actions';
+export {
+    FeedType, FeedFilter, FeedFilterContracts, FeedFilterErrors, FeedFilterTransactions
+} from './sources/feed/feed.selectors';
+export {VortexContract} from './sources/contracts/VortexContract';
